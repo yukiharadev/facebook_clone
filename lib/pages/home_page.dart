a22671e031945b8d2c395aa31ff6d9f457cd9eb9
YukiharaSensei <nguyenhoang.moni03@gmail.com>
2024-03-08 08:52:45 +0700

    update story

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,20 +4,21 @@ import 'package:flutter_app/data/data.dart';
 import 'package:flutter_app/widgets/avatar.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import '../widgets/stories.dart';
 
-import '../widgets/stories_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title:
         SvgPicture.asset(
           "assets/images/logos/facebook_logo.svg",
-          color:Colors.blueAccent,
+          color: Colors.blueAccent,
           height: 23,
         ),
         actions: [
@@ -39,51 +40,36 @@ class HomePage extends StatelessWidget {
         children: [
           Container(
             color: Colors.white,
-            child:Row(
-            children: [
-               Padding(
-                padding: EdgeInsets.all(15.0),
-                child: Avatar(imageUrl: currentUser.imageUrl,),
-              ),
-              const SizedBox(width:10),
-               Expanded(
-                  child: InkWell(
-                    onTap: (){
-                      print("tapped on container");
-                    },
-                    child: Container(
-                      child: Text("Bạn đang nghĩ gì ?"),
-                    ),
-                  )
-              ),
-              IconButton(onPressed: (){}, icon: const Icon(
-                CupertinoIcons.photo_on_rectangle,
-                color: Colors.green,
-                size: 30.0,
-              ),
-              ),
-            ],
-          ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical:10),
-            height: 200,
-            color: Colors.white,
-            child:ListView(
-              scrollDirection: Axis.horizontal,
+            child: Row(
               children: [
-                StoriesCard(),
-                StoriesCard(),
-                StoriesCard(),
-                StoriesCard(),
-                StoriesCard(),
-                StoriesCard(),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Avatar(imageUrl: currentUser.imageUrl,),
+                ),
+                const SizedBox(width: 10),
+                Expanded(
+                    child: InkWell(
+                      onTap: () {
+                        print("tapped on container");
+                      },
+                      child: Container(
+                        child: Text("Bạn đang nghĩ gì ?"),
+                      ),
+                    )
+                ),
+                IconButton(onPressed: () {}, icon: const Icon(
+                  CupertinoIcons.photo_on_rectangle,
+                  color: Colors.green,
+                  size: 30.0,
+                ),
+                ),
               ],
             ),
-          )
+          ),
+          Stories(currentUser: currentUser,
+            stories: stories,)
         ],
       ),
     );
   }
-
 }
